e5ac2ad3bbd24ffadef94be7b6f2cfc1f6e900d5
david <davidissleeping@hotmail.com>
2019-04-18 17:11:53 -0700

    elevaatre

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public GameObject TheElevator;
+    
+        public int height;
+    public float speed;
+ 
+
+    public static int DestFloor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DestFloor = Highlighting.DestFloor;
+        if (TheElevator.transform.position.y < height * (DestFloor - 1) - 0.01F)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        else if (TheElevator.transform.position.y > height * (DestFloor - 1) + 0.01F)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+       
+        
+         
+        
+    }
+}
---
+++ Assets/Scripts/Highlighting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighting : MonoBehaviour
+{
+    public GameObject button;
+    public int FloorNumber;
+    public static int DestFloor;
+        // Start is called before the first frame update
+    void Start()
+    {
+        DestFloor = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown("1"))
+        {
+            button.GetComponent<Renderer>().material.color = new Color32(255, 189, 0, 255);
+            DestFloor = 1;
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            button.GetComponent<Renderer>().material.color = new Color32(255, 255, , 255);
+            DestFloor = 2;
+        }*/
+    }
+
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        button.GetComponent<Renderer>().material.color = new Color32(255, 189, 0, 255);
+        DestFloor = FloorNumber;
+    }
+
+}
